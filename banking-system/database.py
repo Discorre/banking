062a2f0b12466742173d741d6430ed00a96459d4
Discorre <91318121+Discorre@users.noreply.github.com>
2025-03-08 21:27:32 +0700

    Delete banking-system/database.py

--- banking-system/database.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import create_engine, Column, Integer, String, Text
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-
-DATABASE_URL = "postgresql://discorre:0412@localhost/cyberbank"
-
-engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(bind=engine)
-Base = declarative_base()
